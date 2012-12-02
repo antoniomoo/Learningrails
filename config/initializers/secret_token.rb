6867be37183f6790b06360d2f34d12d114349da8
antoniot <adtv26@gmail.com>
2012-12-01 23:35:00 -0430

    Aderi todos los directorios

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello01::Application.config.secret_token = 'd5adb3664e5b9c04321fc037628a93c0cf862c77af572e55077b08dac37f36194322a6060fe1ca24fb0118de22e3f0b39ebe3630eb852ae83afca95cfb54a93a'
